9757373319b1614d36cad69cc47dcea6d1e37efe
Roger Vera <rovera@vassar.edu>
2019-01-19 16:48:20 -0500

    add text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component} from "react";
 import { hot } from "react-hot-loader";
 import "./App.css";
+import styled from 'styled-components'
+
+const Mydiv = styled.div`
+ display:flex;
+ justify-content:center;
+ align-items:center;
+ background-color: papayawhip;
+ height: 100vh;
+`
 
 class App extends Component{
   render(){
     return(
+      <Mydiv>
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1> Banal Platitudes </h1>
       </div>
+      </Mydiv>
     );
   }
 }
